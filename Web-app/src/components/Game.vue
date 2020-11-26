2ff703cf51787b128b48be41e53fc8af658bcc99
Oscar Andres Montes <oscarandresmontes@gmail.com>
2020-11-26 16:29:44 -0500

    Vue Js Components

---
+++ Web-app/src/components/Game.vue
@@ -0,0 +1,6 @@
+<template>
+  <ion-phaser
+      v-bind:game.prop="game"
+      v-bind:initialize.prop="initialize"
+  />
+</template>
---
+++ Web-app/src/components/Init.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card
+      class="mx-auto"
+      max-width="500"
+  >
+    <v-img
+        height="400"
+        :src="require('../assets/logo.png')"
+        class="my-3"
+        contain
+    ></v-img>
+    <v-card-title class="pt-0">Snake Game - Instructions</v-card-title>
+    <v-card-text>
+      <v-row
+          align="center"
+          class="mx-0"
+      >
+        <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">
+          4.5 (410)
+        </div>
+      </v-row>
+      <div>To start the game press the black lines button at the top left of the screen and press Play Game, to see the scores press the highscores button.</div>
+    </v-card-text>
+    <v-card-text>
+      <v-chip-group>
+        <v-chip>Snake</v-chip>
+        <v-chip>Game</v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Init',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
